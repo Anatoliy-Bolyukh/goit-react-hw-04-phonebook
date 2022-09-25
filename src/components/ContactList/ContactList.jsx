15d957a83ab3d647a99decc609cb794c860d1071
user name <106239754+Anatoliy-Bolyukh@users.noreply.github.com>
2022-09-26 01:13:09 +0300

    add commit11

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,25 @@
 import PropTypes from 'prop-types';
 
 const ContactList = ({ contactsList, deleteContact }) => {
+  console.log(contactsList);
   return (
     <div>
       <ul>
-        {contactsList.map(({ id, name, number }) => (
+        {contactsList.map(({ id, name, number }) => {
+          return (
             <li key={id}>
                 <p>{name}: {number}</p>
                 
             <button onClick={() => deleteContact(id)}>delete</button>
           </li>
-        ))}
+        )
+        })}
       </ul>
     </div>
   );
 };
 
 ContactList.propTypes = {
-  // contactsList: PropTypes.func,
   deleteContact: PropTypes.func,
 };
 
